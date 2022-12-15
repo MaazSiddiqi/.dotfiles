ab9d9b3e06e7049c0347df244b6891cfc32e2d91
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-12-15 16:16:21 -0500

    added plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,8 +6,7 @@ local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
 	PACKER_BOOTSTRAP = fn.system({
 		"git",
-		"clone",
-		"--depth",
+		"clone", "--depth",
 		"1",
 		"https://github.com/wbthomason/packer.nvim",
 		install_path,
@@ -54,7 +53,7 @@ return require("packer").startup(function(use)
 
 	--Color Schemes
 	use("shaunsingh/nord.nvim")
-	use("lunarvim/colorschemes")
+	-- use("lunarvim/colorschemes")
 	use("catppuccin/nvim")
 	use("kvrohit/substrata.nvim")
 	use("sainnhe/edge")
@@ -84,7 +83,11 @@ return require("packer").startup(function(use)
 	use("williamboman/nvim-lsp-installer") -- simple to -- use language server installer
 	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 	use("jose-elias-alvarez/nvim-lsp-ts-utils")
-	use("github/copilot.vim") -- Github Copilot.
+	-- use("github/copilot.vim") -- Github Copilot.
+	use({
+		"zbirenbaum/copilot.lua",
+		event = "VimEnter",
+	})
 	use("williamboman/mason.nvim") -- simple to use language server installer
 	use("williamboman/mason-lspconfig.nvim")
 
@@ -146,6 +149,10 @@ return require("packer").startup(function(use)
 	use({ "zbirenbaum/neodim" })
 	use({
 		"numToStr/Comment.nvim",
+	})
+	use({ "windwp/nvim-ts-autotag" })
+	use({
+		"windwp/nvim-autopairs",
 	})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
