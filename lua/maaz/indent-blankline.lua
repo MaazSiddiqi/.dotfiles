7c6b1958ff7409716d51f1157cb6678cbb874e57
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-12-22 13:02:14 -0500

    saving init.lua

--- lua/maaz/indent-blankline.lua
+++ lua/maaz/indent-blankline.lua
@@ -1,6 +1,5 @@
 require("indent_blankline").setup({
 	use_treesitter = true,
-  show_first_indent_level = false,
-  show_current_context = true,
-  
+	show_first_indent_level = false,
+	show_current_context = true,
 })
