a4851f0fced1a273d3259ed84f51db962a5abb84
Maaz Ali <maazali22@gmail.com>
2023-02-22 22:43:04 -0500

    added toggleterm

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -9,9 +9,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      defaults = {
-        keymaps = false, -- lazyvim.config.keymaps
-      },
+      defaults = { keymaps = false, options = false },
     },
   },
 }
