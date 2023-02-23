43b66f05df5a8d6f3c3f4335ed180b86da3595a6
Maaz Ali <maazali22@gmail.com>
2023-02-22 22:43:41 -0500

    saved nvim profile

--- _nvims/lazyNvim/lua/plugins/lsp.lua
+++ _nvims/lazyNvim/lua/plugins/lsp.lua
@@ -1,12 +1,11 @@
 -- add pyright to lspconfig
 return {
   "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
   opts = {
-    ---@type lspconfig.options
-    servers = {
-      -- pyright will be automatically installed with mason and loaded with lspconfig
-      pyright = {},
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
     },
   },
 }
