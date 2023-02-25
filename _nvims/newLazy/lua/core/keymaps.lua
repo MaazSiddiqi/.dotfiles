8943d238b2c2fad594f31767e0a7594f7f5d8f11
Maaz Ali <maazali22@gmail.com>
2023-02-25 15:56:06 -0500

    formatting

--- _nvims/newLazy/lua/core/keymaps.lua
+++ _nvims/newLazy/lua/core/keymaps.lua
@@ -1,66 +1,65 @@
 local function map(mode, lhs, rhs, opts)
-    vim.keymap.set(mode, lhs, rhs, opts)
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 map("i", "jk", "<ESC>", { desc = "Enter normal mode" })
 
 -- better up/down
 map("n", "j", "v:count == 0 ? 'gj' : 'j'", {
-    expr = true,
-    silent = true
+	expr = true,
+	silent = true,
 })
 map("n", "k", "v:count == 0 ? 'gk' : 'k'", {
-    expr = true,
-    silent = true
+	expr = true,
+	silent = true,
 })
-
 
 -- Move to window using the <ctrl> hjkl keys
 map("n", "<C-h>", "<C-w>h", {
-    desc = "Go to left window"
+	desc = "Go to left window",
 })
 map("n", "<C-j>", "<C-w>j", {
-    desc = "Go to lower window"
+	desc = "Go to lower window",
 })
 map("n", "<C-k>", "<C-w>k", {
-    desc = "Go to upper window"
+	desc = "Go to upper window",
 })
 map("n", "<C-l>", "<C-w>l", {
-    desc = "Go to right window"
+	desc = "Go to right window",
 })
 
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-Up>", "<cmd>resize +2<cr>", {
-    desc = "Increase window height"
+	desc = "Increase window height",
 })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", {
-    desc = "Decrease window height"
+	desc = "Decrease window height",
 })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", {
-    desc = "Decrease window width"
+	desc = "Decrease window width",
 })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", {
-    desc = "Increase window width"
+	desc = "Increase window width",
 })
 
 -- Move Lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", {
-    desc = "Move down"
+	desc = "Move down",
 })
 map("n", "<A-k>", "<cmd>m .-2<cr>==", {
-    desc = "Move up"
+	desc = "Move up",
 })
 map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", {
-    desc = "Move down"
+	desc = "Move down",
 })
 map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", {
-    desc = "Move up"
+	desc = "Move up",
 })
 map("v", "<A-j>", ":m '>+1<cr>gv=gv", {
-    desc = "Move down"
+	desc = "Move down",
 })
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", {
-    desc = "Move up"
+	desc = "Move up",
 })
 
 -- buffers
@@ -77,16 +76,16 @@ map("v", "<A-k>", ":m '<-2<cr>gv=gv", {
 --     desc = "Next buffer"
 -- })
 map("n", "<S-h>", "<cmd>bprevious<cr>", {
-    desc = "Prev buffer"
+	desc = "Prev buffer",
 })
 map("n", "<S-l>", "<cmd>bnext<cr>", {
-    desc = "Next buffer"
+	desc = "Next buffer",
 })
 map("n", "[b", "<cmd>bprevious<cr>", {
-    desc = "Prev buffer"
+	desc = "Prev buffer",
 })
 map("n", "]b", "<cmd>bnext<cr>", {
-    desc = "Next buffer"
+	desc = "Next buffer",
 })
 -- map("n", "<leader>bb", "<cmd>e #<cr>", {
 --     desc = "Switch to Other Buffer"
@@ -97,13 +96,13 @@ map("n", "]b", "<cmd>bnext<cr>", {
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", {
-    desc = "Escape and clear hlsearch"
+	desc = "Escape and clear hlsearch",
 })
 
 -- Clear search, diff update and redraw
 -- taken from runtime/lua/_editor.lua
 map("n", "<leader>ur", "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>", {
-    desc = "Redraw / clear hlsearch / diff update"
+	desc = "Redraw / clear hlsearch / diff update",
 })
 
 map("n", "gw", "*N")
@@ -111,28 +110,28 @@ map("x", "gw", "*N")
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", {
-    expr = true,
-    desc = "Next search result"
+	expr = true,
+	desc = "Next search result",
 })
 map("x", "n", "'Nn'[v:searchforward]", {
-    expr = true,
-    desc = "Next search result"
+	expr = true,
+	desc = "Next search result",
 })
 map("o", "n", "'Nn'[v:searchforward]", {
-    expr = true,
-    desc = "Next search result"
+	expr = true,
+	desc = "Next search result",
 })
 map("n", "N", "'nN'[v:searchforward]", {
-    expr = true,
-    desc = "Prev search result"
+	expr = true,
+	desc = "Prev search result",
 })
 map("x", "N", "'nN'[v:searchforward]", {
-    expr = true,
-    desc = "Prev search result"
+	expr = true,
+	desc = "Prev search result",
 })
 map("o", "N", "'nN'[v:searchforward]", {
-    expr = true,
-    desc = "Prev search result"
+	expr = true,
+	desc = "Prev search result",
 })
 
 -- better indenting
@@ -141,16 +140,15 @@ map("v", ">", ">gv")
 
 -- lazy
 map("n", "<leader>L", "<cmd>Lazy<cr>", {
-    desc = "Lazy"
+	desc = "Lazy",
 })
 
 map("n", "<leader>xl", "<cmd>lopen<cr>", {
-    desc = "Location List"
+	desc = "Location List",
 })
 map("n", "<leader>xq", "<cmd>copen<cr>", {
-    desc = "Quickfix List"
+	desc = "Quickfix List",
 })
-
 
 -- -- lazygit
 -- map("n", "<leader>gg", function()
@@ -168,39 +166,37 @@ map("n", "<leader>xq", "<cmd>copen<cr>", {
 
 -- windows
 map("n", "<leader>ww", "<C-W>p", {
-    desc = "Other window"
+	desc = "Other window",
 })
 map("n", "<leader>wd", "<C-W>c", {
-    desc = "Delete window"
+	desc = "Delete window",
 })
 map("n", "<leader>wj", "<C-W>s", {
-    desc = "Split window below"
+	desc = "Split window below",
 })
 map("n", "<leader>wl", "<C-W>v", {
-    desc = "Split window right"
+	desc = "Split window right",
 })
 
 -- tabs
 map("n", "<leader><tab>l", "<cmd>tablast<cr>", {
-    desc = "Last Tab"
+	desc = "Last Tab",
 })
 map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", {
-    desc = "First Tab"
+	desc = "First Tab",
 })
 map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", {
-    desc = "New Tab"
+	desc = "New Tab",
 })
 map("n", "<leader><tab>d", "<cmd>tabclose<cr>", {
-    desc = "Close Tab"
+	desc = "Close Tab",
 })
 map("n", "<leader>]<tab>", "<cmd>tabnext<cr>", {
-    desc = "Next Tab"
+	desc = "Next Tab",
 })
 map("n", "<leader>[<tab>", "<cmd>tabprevious<cr>", {
-    desc = "Previous Tab"
+	desc = "Previous Tab",
 })
-
-
 
 map("i", "<Tab>", 'copilot#Accept("")', { desc = "Accept copilot suggestion", silent = true, expr = true })
 
@@ -222,7 +218,9 @@ map("n", "<leader><leader>;", '<cmd>w|%bd|e#|bd!#|normal`"<CR><CR>', { silent = 
 map("n", "Y", "y$", { silent = true })
 --
 -- lazygit
-map("n", "<leader>G", function() Util.float_term({ "lazygit" }) end, { desc = "Open LazyGit"})
+map("n", "<leader>G", function()
+	Util.float_term({ "lazygit" })
+end, { desc = "Open LazyGit" })
 -- lazy
 map("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "LSP buffer info" })
--- _nvims/newLazy/lua/core/options.lua
+++ _nvims/newLazy/lua/core/options.lua
@@ -24,20 +24,20 @@ opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = {"buffers", "curdir", "tabpages", "winsize"}
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append{
-    W = true,
-    I = true,
-    c = true
-}
+opt.shortmess:append({
+	W = true,
+	I = true,
+	c = true,
+})
 opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
-opt.spelllang = {"en"}
+opt.spelllang = { "en" }
 opt.splitbelow = true -- Put new windows below current
 opt.splitright = true -- Put new windows right of current
 opt.tabstop = 2 -- Number of spaces tabs count for
@@ -51,10 +51,10 @@ opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
 
 if vim.fn.has("nvim-0.9.0") == 1 then
-    opt.splitkeep = "screen"
-    opt.shortmess:append{
-        C = true
-    }
+	opt.splitkeep = "screen"
+	opt.shortmess:append({
+		C = true,
+	})
 end
 
 -- Fix markdown indentation settings
