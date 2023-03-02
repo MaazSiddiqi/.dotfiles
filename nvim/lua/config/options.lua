7ea20020391a2e928427358d95419e39f8406662
Maaz Ali <maazali22@gmail.com>
2023-03-02 18:26:42 -0500

    added neodim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -24,20 +24,20 @@ opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = {"buffers", "curdir", "tabpages", "winsize"}
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append{
-    W = true,
-    I = true,
-    c = true
-}
+opt.shortmess:append({
+  W = true,
+  I = true,
+  c = true,
+})
 opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
-opt.spelllang = {"en"}
+opt.spelllang = { "en" }
 opt.splitbelow = true -- Put new windows below current
 opt.splitright = true -- Put new windows right of current
 opt.tabstop = 2 -- Number of spaces tabs count for
@@ -49,12 +49,14 @@ opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
+opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait400-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175" -- Gui cursor
 
 if vim.fn.has("nvim-0.9.0") == 1 then
-    opt.splitkeep = "screen"
-    opt.shortmess:append{
-        C = true
-    }
+  opt.splitkeep = "screen"
+  opt.shortmess:append({
+    C = true,
+  })
 end
 
 -- Fix markdown indentation settings
---
+++ nvim/lua/plugins/neodim.lua
@@ -0,0 +1,3 @@
+return {
+  "zbirenbaum/neodim",
+}
