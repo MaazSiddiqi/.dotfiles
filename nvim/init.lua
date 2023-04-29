c4683aba6c085ebe732fdc8835a2fe67e30ad838
Maaz Ali <maazali22@gmail.com>
2023-04-29 16:36:55 -0400

    fixed colorscheme autoload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,13 +12,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
+-- load config
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 
-
-opts = {}
+-- load plugins
 require("lazy").setup({
 	{import = "plugins"}
-}, opts)
+}, {
+  install = {
+    missing = true,
+    colorscheme = {"catpuccin"},
+  }
+})
+
+vim.cmd.colorscheme("catppuccin")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -53,3 +53,4 @@ end
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
+
