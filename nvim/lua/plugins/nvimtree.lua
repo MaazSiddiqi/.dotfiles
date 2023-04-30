d65b5f2a95ecfcbc1caa7b616679c0cad8538389
Maaz Ali <maazali22@gmail.com>
2023-04-30 00:36:00 -0400

    setup nvim-tree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,9 +1,23 @@
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
-  opts = {},
+  opts = {
+    on_attach = function(bufnr)
+      local api = require('nvim-tree.api')
+
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+
+
+      vim.keymap.set('n', 'l',  api.node.open.edit,                    opts('Open'))
+      vim.keymap.set('n', 'h',  api.node.navigate.parent_close,        opts('Close Directory'))
+    end,
+  },
   keys = {
-    {"<leader>e", "<cmd>NvimTreeToggle<cr>"}
+    {"<leader>e", "<cmd>NvimTreeToggle<cr>", {silent = true} }
   },
   config = true
 }
