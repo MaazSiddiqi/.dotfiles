a0d8bc6102855138754d5a4aeda051f8f74d5298
Maaz Ali <maazali22@gmail.com>
2023-04-29 23:57:28 -0400

    disable netrw

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
