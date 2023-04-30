6f5791f392ac3f2f9bf72278de0130c13b8b43fe
Maaz Ali <maazali22@gmail.com>
2023-04-29 23:49:28 -0400

    fixed navic and icons

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -166,13 +166,23 @@ return {
       --     require("nvim-navic").attach(client, buffer)
       --   end
       -- end)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local buffer = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+          if client.server_capabilities.documentSymbolProvider then
+            require("nvim-navic").attach(client, buffer)
+          end
+        end,
+      })
     end,
     opts = function()
       return {
         separator = " ",
         highlight = true,
         depth_limit = 5,
-        -- icons = require("lazyvim.config").icons.kinds,
+        icons = require("config.icons").kinds,
       }
     end,
   },
