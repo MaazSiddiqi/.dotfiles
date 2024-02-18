77230238572d48c36391a03eafa966995903080f
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-02-17 23:53:52 -0500

    create bootstrap script to download repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,21 +5,26 @@ echo "Bootstrapping dotfiles..."
 echo "========================="
 echo ""
 
-# echo "Checking Homebrew..."
-# which -s brew
-# if [[ $? != 0 ]] ; then
-#     echo "Homebrew not found, installing:"
-#     # Install Homebrew
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# else
-#     echo "Homebrew found, updating:"
-#     brew update
-# fi
-#
-
-if [[ -e ~/.dotfile ]]; then
-  echo "[INFO] Backing up old dotfiles"
-  mv ~/.dotfile ~/.dotfile.bak 
+echo "[INFO] Checking Homebrew..."
+which -s brew
+if [[ $? != 0 ]]; then
+	echo "[INFO] Homebrew not found, installing now."
+	# Install Homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "[INFO] Homebrew found, updating now"
+	brew update
 fi
 
-git clone https://github.com/MaazSiddiqi/.dotfiles.git ~/.dotfile
+if [[ -e ~/.dotfiles ]]; then
+	echo "[INFO] Backing up old dotfiles to ~/.dotfiles.bak"
+	mv ~/.dotfiles ~/.dotfiles.bak
+fi
+
+echo "[INFO] Downloading dotfiles to ~/.dotfiles"
+git clone https://github.com/MaazSiddiqi/.dotfiles.git ~/.dotfiles
+
+echo ""
+echo "======================"
+echo "Finished Bootstrapping"
+echo "======================"
