5aa14633b8ce2566f51cef7aa66f5cf753641fc2
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-02-17 21:56:20 -0500

    update brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ echo "Bootstrapping dotfiles..."
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim/ ~/.config/nvim
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/wezterm/ ~/.config/wezterm
 
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
