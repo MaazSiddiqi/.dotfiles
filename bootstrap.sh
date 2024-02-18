558acb2ea579d6b171baa3dda850d72539b58009
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-02-17 23:22:24 -0500

    add git command

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,25 @@
-echo "Bootstrapping dotfiles..."
+#!/bin/bash
 
+echo "========================="
+echo "Bootstrapping dotfiles..."
+echo "========================="
+echo ""
+
+# echo "Checking Homebrew..."
+# which -s brew
+# if [[ $? != 0 ]] ; then
+#     echo "Homebrew not found, installing:"
+#     # Install Homebrew
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# else
+#     echo "Homebrew found, updating:"
+#     brew update
+# fi
+#
+
+if [[ -e ~/.dotfile ]]; then
+  echo "[INFO] Backing up old dotfiles"
+  mv ~/.dotfile ~/.dotfile.bak 
+fi
+
+git clone https://github.com/MaazSiddiqi/.dotfiles.git ~/.dotfile
