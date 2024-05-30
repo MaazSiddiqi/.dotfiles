0e56de2b657ca644f2db3a077eeb4ea692465808
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-05-30 09:35:11 -0400

    reduce timeout

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank({
       higroup = "IncSearch",
-      timeout = 40,
+      timeout = 20,
     })
   end,
 })
