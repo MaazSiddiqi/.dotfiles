ac529f1ba4fe033ac391d55cb144a6c99f11e500
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-08-28 23:06:05 -0400

    formatting

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,17 +8,17 @@ echo ""
 echo "[INFO] Checking Homebrew..."
 which -s brew
 if [[ $? != 0 ]]; then
-	echo "[INFO] Homebrew not found, installing now."
-	# Install Homebrew
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "[INFO] Homebrew not found, installing now."
+  # Install Homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-	echo "[INFO] Homebrew found, updating now"
-	brew update
+  echo "[INFO] Homebrew found, updating now"
+  brew update
 fi
 
 if [[ -e ~/.dotfiles ]]; then
-	echo "[INFO] Backing up old dotfiles to ~/.dotfiles.bak"
-	mv ~/.dotfiles ~/.dotfiles.bak
+  echo "[INFO] Backing up old dotfiles to ~/.dotfiles.bak"
+  mv ~/.dotfiles ~/.dotfiles.bak
 fi
 
 echo "[INFO] Downloading dotfiles to ~/.dotfiles"
