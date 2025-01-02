dc118ee3c317c209e5d40136ee004cb19a2669f4
Maaz Ali Siddiqi <msidd54@uwo.ca>
2025-01-01 22:06:39 -0500

    refactor

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,57 @@
+return {
+  -- A file explorer tree for neovim written in lua
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    opts = {
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file
+            api.node.open.edit()
+            -- Close the tree if file was opened
+            api.tree.close()
+          end
+        end
+
+        -- open as vsplit on current node
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+
+          if node.nodes ~= nil then
+            -- expand or collapse folder
+            api.node.open.edit()
+          else
+            -- open file as vsplit
+            api.node.open.vertical()
+          end
+
+          -- Finally refocus on tree if it was lost
+          api.tree.focus()
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+        vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+        vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+        vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+      end,
+    },
+    keys = {
+      { "<leader>E", "<cmd>NvimTreeToggle<cr>", { silent = true } },
+      { "<leader>e", "<cmd>NvimTreeFindFile<cr>", { silent = true } },
+    },
+    config = true,
+  },
+}
